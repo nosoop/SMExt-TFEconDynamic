d79187aaf5c374b2cbd35da949808ea0f12784ff
nosoop <nosoop@users.noreply.github.com>
2023-03-25 07:13:24 -0700

    Reset version number
    This version number was from the hardcoded MM:S version.

--- smsdk_config.h
+++ smsdk_config.h
@@ -40,7 +40,7 @@
 /* Basic information exposed publicly */
 #define SMEXT_CONF_NAME			"TF2 Econ Dynamic"
 #define SMEXT_CONF_DESCRIPTION	"Injects user-defined content into the game schema"
-#define SMEXT_CONF_VERSION		"1.3.0"
+#define SMEXT_CONF_VERSION		"0.0.1"
 #define SMEXT_CONF_AUTHOR		"nosoop"
 #define SMEXT_CONF_URL			"https://github.com/nosoop/SMExt-TFEconDynamic"
 #define SMEXT_CONF_LOGTAG		"dynschema"
